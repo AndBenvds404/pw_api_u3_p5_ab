13609555d3d1d8059f330882ac43fb86b60308f9
AndBenvds404 <andres_benavides17@hotmail.com>
2024-01-31 19:43:16 -0500

    taller28

--- src/main/java/com/example/pw_api_u3_p5_ab/controller/EstudianteControllerRestful.java
+++ src/main/java/com/example/pw_api_u3_p5_ab/controller/EstudianteControllerRestful.java
@@ -7,26 +7,57 @@ import com.example.pw_api_u3_p5_ab.service.IEstudianteService;
  
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
  
 //API: por el proyecto
-//servicio -> controller: clase controller
-@RestController//Servicio
+
+@RestController
+
+//Path clase = Servicio
 @RequestMapping(path="/estudiantes")
 public class EstudianteControllerRestFul {
  
     @Autowired
     private IEstudianteService estudianteService;
  
+
+
+    //Metodosd = capacidades
+
     @GetMapping(path="/buscar")  
     public Estudiante buscar(){
-        return estudianteService.buscar(1);
+        return estudianteService.buscar(2);
     }
  
+   // http://localhost:8080/API/v1.0/Matricula/estudiantes/buscar
+
     @PostMapping(path="/guardar")
-    public void guardar(Estudiante estudiante){
+    public void guardar(@RequestBody Estudiante estudiante){
         this.estudianteService.guardar(estudiante);
+    }
+
+    @PutMapping(path="/actualizar")
+    public void actualizar(@RequestBody Estudiante estudiante){
+        this.estudianteService.actualizar(estudiante);
+    }
+
+
+    @PatchMapping(path = "/actualizarParcial")
+    public void actualizarParcial (@RequestBody Estudiante estudiante){
+
+        this.estudianteService.actualizarParcial(estudiante.getApellido(), estudiante.getNombre(), estudiante.getId());
+
+    }
+
+
+    @DeleteMapping(path = "/borrar")
+    public void borrar (){
+        this.estudianteService.borrar(1);
     }
  
    
--- src/main/java/com/example/pw_api_u3_p5_ab/repository/modelo/Estudiante.java
+++ src/main/java/com/example/pw_api_u3_p5_ab/repository/modelo/Estudiante.java
@@ -2,6 +2,8 @@ package com.example.pw_api_u3_p5_ab.repository.modelo;
 
 import java.time.LocalDateTime;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "estudiante")
+
 public class Estudiante {
 
     @Id
